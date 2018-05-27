50bb774d7351b958d065fa2fdb090081864ff1f2
Alexei Tipenko <alexeitipenko@cmail.carleton.ca>
2018-05-27 15:42:09 +0000

    converted output_1.png and R_1.png to text files

--- ocr-test/ocr.R
+++ ocr-test/ocr.R
@@ -6,9 +6,17 @@ library(magick)
 library(magrittr)
 library(pdftools)
 library(tesseract)
-text <- image_read("~/module2repo/war-diary/e001518087.jpg") %>% 
+
+text <- image_read("~/module2repo/ocr-test/R_1.png") %>% 
   image_resize("2000") %>% 
   image_convert(colorspace = 'gray') %>% 
   image_trim() %>% 
   image_ocr()
-write.table(text, "~/R.txt")
+write.table(text, "~/R_1.txt")
+
+# text <- image_read("~/module2repo/war-diary/e001518087.jpg") %>% 
+#   image_resize("2000") %>% 
+#   image_convert(colorspace = 'gray') %>% 
+#   image_trim() %>% 
+#   image_ocr()
+# write.table(text, "~/R.txt")
