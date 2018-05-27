f7a5ed1bbb0602a544cf23839b90b1f8bc156872
Alexei Tipenko <alexeitipenko@cmail.carleton.ca>
2018-05-27 15:01:22 +0000

    Moved ocr.R file into ocr-test directory

---
+++ ocr-test/ocr.R
@@ -0,0 +1,14 @@
+install.packages('magick')
+install.packages('magrittr')
+install.packages('pdftools')
+install.packages('tesseract')
+library(magick) 
+library(magrittr)
+library(pdftools)
+library(tesseract)
+text <- image_read("~/module2repo/war-diary/e001518087.jpg") %>% 
+  image_resize("2000") %>% 
+  image_convert(colorspace = 'gray') %>% 
+  image_trim() %>% 
+  image_ocr()
+write.table(text, "~/R.txt")
